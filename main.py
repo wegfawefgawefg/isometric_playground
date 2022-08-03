fdc984d3f6e419424b3937a3809c03b737720d34
wegfawefgawefg <668es218pur@gmail.com>
2022-08-04 01:09:57 +0900

    more iso

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from enum import Enum, auto
-from functools import lru_cache
-import os
-
 import math
 from pprint import pprint
 import random
@@ -15,6 +11,8 @@ from pygame.locals import (
     QUIT,
 )
 
+import tile_consts
+
 TILE_HEIGHT = 17
 TILE_WIDTH = 16
 TILE_SIZE = 16
@@ -25,7 +23,7 @@ ATLAS_HEIGHT = 11
 ATLAS_WIDTH = 11
 NUM_TILE_TYPES = ATLAS_HEIGHT * ATLAS_WIDTH
 
-SCREEN_DIMS = Vector2(240, 160)*1.2
+SCREEN_DIMS = Vector2(240, 160)*2.0
 WINDOW_DIMS = SCREEN_DIMS * 3.0
 
 VIEWPORT_SIZE = 16
@@ -59,17 +57,58 @@ def main():
     clock = pygame.time.Clock()
     running = True
 
-    tiles = []
+    paths = list(tile_consts.DIRT_PATHS.values()) + \
+        list(tile_consts.HALF_DIRT_PATHS.values())
+
+    # tiles = []
+    # i = 0
+    # for y in range(VIEWPORT_SIZE):
+    #     row = []
+    #     for x in range(VIEWPORT_SIZE):
+    #         row.append(random.choice((paths)))
+    #         # row.append(random.choice([0, 1, 11, 12]))
+    #         # row.append(i)
+    #         # row.append(1)
+    #         # row.append(random.randint(0, 40))
+    #         i += 1
+    #     tiles.append(row)
+
+    top_layer_block_ids = [tile_consts.NAME_TO_INDEX[n]
+                           for n in ["GRASS", "HALF_GRASS", "DIRT", "HALF_DIRT"]]
+    inner_blocks_ids = [tile_consts.NAME_TO_INDEX[n]
+                        for n in ["STONE", "DIRT", "HALF_DIRT", "HALF_STONE"]]
+    lower_blocks_ids = [tile_consts.NAME_TO_INDEX[n]
+                        for n in ["STONE", "HALF_STONE", "LAVA", "HALF_LAVA"]]
+
+    stone_brick_ids = [tile_consts.NAME_TO_INDEX[n]
+                       for n in ["STONE_BRICK"]]
+
+    blocks = []
     i = 0
-    for y in range(VIEWPORT_SIZE):
-        row = []
-        for x in range(VIEWPORT_SIZE):
-            row.append(random.choice([0, 1, 11, 12]))
-            # row.append(i)
-            # row.append(1)
-            # row.append(random.randint(0, 40))
-            i += 1
-        tiles.append(row)
+    for z in range(VIEWPORT_SIZE):
+        zrow = []
+        for y in range(VIEWPORT_SIZE):
+            row = []
+            for x in range(VIEWPORT_SIZE):
+                if (VIEWPORT_SIZE - 2) < z < VIEWPORT_SIZE:
+                    tile = random.choice(top_layer_block_ids)
+                elif (VIEWPORT_SIZE - 3) < z <= (VIEWPORT_SIZE - 1):
+                    tile = random.choice(stone_brick_ids)
+                elif 2 < z <= 7:
+                    tile = random.choice(inner_blocks_ids)
+                elif z <= 2:
+                    tile = random.choice(lower_blocks_ids)
+                elif (x == 0) or (x == VIEWPORT_SIZE - 1) or (y == 0):
+                    tile = random.choice(stone_brick_ids)
+                else:
+                    tile = -1
+
+                row.append(tile)
+                # row.append(i)
+                # row.append(random.choice(2))
+                i += 1
+            zrow.append(row)
+        blocks.append(zrow)
 
     while running:
         for event in pygame.event.get():
@@ -79,26 +118,54 @@ def main():
                 running = False
         screen.fill((0, 0, 0))
 
+        # t = pygame.time.get_ticks()
+        # flex_factor = 0.5
+        # start_x, start_y = SCREEN_DIMS.x / 2, SCREEN_DIMS.y / 8
+        # for y in range(VIEWPORT_SIZE):
+        #     for x in range(VIEWPORT_SIZE):
+        #         iso_coords = to_iso_coords(x, y)
+        #         screen.blit(
+        #             tiles_im, (
+        #                 start_x + iso_coords[0],
+        #                 start_y + iso_coords[1]
+        #                 + (TILE_SIZE//1)
+        #                 * -(math.sin(
+        #                     x * flex_factor
+        #                     + y * flex_factor // 8
+        #                     + t*0.008
+        #                 )/2.0 + 0.5)
+        #                 * ((math.sin(2.0+t*0.004)/2.0 + 0.5)**2)
+        #             ),
+        #             get_tile_area(tiles[y][x])
+        #         )
+
         t = pygame.time.get_ticks()
         flex_factor = 0.5
-        start_x, start_y = SCREEN_DIMS.x / 2, SCREEN_DIMS.y / 8
-        for y in range(VIEWPORT_SIZE):
-            for x in range(VIEWPORT_SIZE):
-                iso_coords = to_iso_coords(x, y)
-                screen.blit(
-                    tiles_im, (
-                        start_x + iso_coords[0],
-                        start_y + iso_coords[1]
-                        + (TILE_SIZE//1)
-                        * -(math.sin(
-                            x * flex_factor
-                            + y * flex_factor // 8
-                            + t*0.008
-                        )/2.0 + 0.5)
-                        * ((math.sin(2.0+t*0.004)/2.0 + 0.5)**2)
-                    ),
-                    get_tile_area(tiles[y][x])
-                )
+        start_x, start_y = (
+            SCREEN_DIMS.x / 2,
+            SCREEN_DIMS.y / 10 + (VIEWPORT_SIZE * TILE_SIZE) / 2)
+        for z in range(VIEWPORT_SIZE):
+            for y in range(VIEWPORT_SIZE):
+                for x in range(VIEWPORT_SIZE):
+                    iso_coords = to_iso_coords(x, y)
+                    block = blocks[z][y][x]
+                    if not block == -1:
+                        screen.blit(
+                            tiles_im, (
+                                start_x + iso_coords[0],
+                                start_y + iso_coords[1]
+                                - z * TILE_SIZE // 2
+                                + (TILE_SIZE//1)
+                                * -(math.sin(
+                                    x * flex_factor
+                                    + y * flex_factor // 8
+                                    + t*0.008
+                                )/2.0 + 0.5)
+                                * ((math.sin(2.0+t*0.004)/2.0 + 0.5)**2)
+                            ),
+                            get_tile_area(blocks[z][y][x])
+                        )
+
         # mt = pygame.mouse.get_pos()
         # m = (Vector2(mt[0], mt[1]).elementwise() /
         #      WINDOW_DIMS).elementwise() * SCREEN_DIMS
